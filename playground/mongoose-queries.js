b851efc0c4b28ae8382e87f9dfbdb85545911c87
Cheran Sam Raj <a-cheransr@SINGTELCDC.COM>
2019-01-20 17:52:29 +0530

    Add tests for GET /todos

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -5,6 +5,12 @@ const {Users} = require('./../server/model/users');
 
 var id = '5c41c200b9686112d4d1a33e';
 
+//to check id is valid 
+// if(!ObjectID.isValid(id)) {
+//     console.log('ID is not valid');
+// }
+
+
 //To show the array object
 //Users.find()
 //To show the object by one field
