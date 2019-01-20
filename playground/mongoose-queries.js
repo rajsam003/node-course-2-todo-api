854e3bb58f0a352b6c2ef05957083868ee4ed743
Cheran Sam Raj <a-cheransr@SINGTELCDC.COM>
2019-01-20 17:45:40 +0530

    Add tests for GET /todos

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -5,6 +5,11 @@ const {Users} = require('./../server/model/users');
 
 var id = '5c41c200b9686112d4d1a33e';
 
+//To show the array object
+//Users.find()
+//To show the object by one field
+//Users.findByOne
+//To show the object by ID
 Users.findById(id).then((user) => {
     if(!user){
         return console.log('ID not found');
