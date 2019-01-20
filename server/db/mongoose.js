1d7f8eab5249be454739b90a3caff71d69c2fdd5
Cheran Sam Raj <a-cheransr@SINGTELCDC.COM>
2019-01-20 19:31:05 +0530

    Adding application to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect('mongodb://SamRaj:ur12cs206@ds161764.mlab.com:61764/nodedb'||'mongodb://localhost:27017/TodoApp');
 
 module.exports ={mongoose};
